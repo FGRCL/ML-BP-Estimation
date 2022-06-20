a3441f63417b5ff6b44b725bf07088a322334402
Franccois <FGRCLaBerge@gmail.com>
2022-06-20 13:15:08 -0700

    activate environment properly

--- train.sh
+++ train.sh
@@ -6,13 +6,13 @@
 #SBATCH --gres=gpu:v100l:1               # Number of GPUs (per node)
 #SBATCH --cpus-per-task=8          # Number of cores (not cpus)
 #SBATCH --mem=32G               # memory (per node)
-#SBATCH --time=1-00:00             # time (DD-HH:MM)
+#SBATCH --time=0-24:00             # time (DD-HH:MM)
 
 # Setup and run task -------------
 module restore ml
 module load gcc/9.3.0 arrow/8.0.0
 virtualenv --no-download env
-source /env/bin/activate
+source env/bin/activate
 pip install -r requirements.txt
 
 python -m train.py
