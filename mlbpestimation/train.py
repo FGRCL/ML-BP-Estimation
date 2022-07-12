cffeb95d7f436949300dbd39543c435371c2cca9
François LaBerge <fgrclaberge@gmail.com>
2022-07-12 15:44:16 -0400

    add wandb and add main to train.py

--- mlbpestimation/train.py
+++ mlbpestimation/train.py
@@ -6,19 +6,25 @@ from mlbpestimation.metrics.standardeviation import AbsoluteError, StandardDevia
 from mlbpestimation.models.heartbeat import build_heartbeat_cnn_model
 from mlbpestimation.vitaldb.casesplit import load_vitaldb_dataset
 
-epochs = 10
 
-train, val, _ = load_vitaldb_dataset()
-(train, val), model = build_heartbeat_cnn_model([train, val])
+def main():
+    epochs = 10
 
-model.summary()
-model.compile(optimizer='Adam', loss=keras.losses.MeanSquaredError(),
-              metrics=[
-                  MeanAbsoluteError(),
-                  StandardDeviation(AbsoluteError())
-              ]
-              )
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="../logs", histogram_freq=1, profile_batch='500,520')
-model.fit(train, epochs=epochs, callbacks=[tensorboard_callback], validation_data=val)
+    train, val, _ = load_vitaldb_dataset()
+    (train, val), model = build_heartbeat_cnn_model([train, val])
 
-model.save('models')
+    model.summary()
+    model.compile(optimizer='Adam', loss=keras.losses.MeanSquaredError(),
+                  metrics=[
+                      MeanAbsoluteError(),
+                      StandardDeviation(AbsoluteError())
+                  ]
+                  )
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="../logs", histogram_freq=1, profile_batch='500,520')
+    model.fit(train, epochs=epochs, callbacks=[tensorboard_callback], validation_data=val)
+
+    model.save('models')
+
+
+if __name__ == '__main__':
+    main()
