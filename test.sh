44a8a72d82a0e228f8f8ca64bf2e887afa5c63bf
François LaBerge <fgrclaberge@gmail.com>
2022-07-20 23:22:08 -0400

    increase memory for training

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@
 #SBATCH --account def-bentahar
 #SBATCH --gres=gpu:v100l:1               # Number of GPUs (per node)
 #SBATCH --cpus-per-task=8          # Number of cores (not cpus)
-#SBATCH --mem=32G               # memory (per node)
+#SBATCH --mem=64G               # memory (per node)
 #SBATCH --time=0-02:00             # time (DD-HH:MM)
 
 # Setup and run task -------------
