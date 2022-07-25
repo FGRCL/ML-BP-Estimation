06a83c73eb1592c06d631cbe7e5d010b877ab55b
François LaBerge <fgrclaberge@gmail.com>
2022-07-25 16:38:49 -0400

    Add tests for training with the mimic dataset

--- mlbpestimation/configuration.py
+++
@@ -1,9 +0,0 @@
-from envyaml import EnvYAML
-
-from mlbpestimation.resources.importer import get_resource
-
-CONFIG_FILE_NAME = 'config.yaml'
-
-config_file = get_resource(CONFIG_FILE_NAME)
-with open(config_file, 'r') as config:
-    configuration = EnvYAML(config.name)
