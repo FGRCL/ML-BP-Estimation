c26c2fce8c154226adcd1ff899c2a7b44191b082
François LaBerge <fgrclaberge@gmail.com>
2022-08-02 15:57:07 -0400

    add config for wandb

--- train.py
+++ train.py
@@ -1,25 +1,18 @@
-import tensorflow as tf
 from keras.metrics import MeanAbsoluteError
 from tensorflow import keras
 from wandb.integration.keras import WandbCallback
 
-from wandb import init
-
-from mlbpestimation.metrics.standardeviation import StandardDeviation, AbsoluteError
+from mlbpestimation.configuration import configuration
+from mlbpestimation.metrics.standardeviation import AbsoluteError, StandardDeviation
 from mlbpestimation.models.heartbeat import build_heartbeat_cnn_model
 from mlbpestimation.vitaldb.casesplit import load_vitaldb_dataset
+from wandb import init
 
 
 def main():
     epochs = 10
-    config = {
-        "learning_rate": 0.001,
-        "epochs": 100,
-        "batch_size": 128
-    }
-    init(project="test-project", entity="warpfive", config=config)
-
-
+    init(project=configuration['wandb.project_name'], entity=configuration['wandb.entity'],
+         config=configuration['wandb.config'])
 
     train, val, _ = load_vitaldb_dataset()
     (train, val), model = build_heartbeat_cnn_model([train, val])
