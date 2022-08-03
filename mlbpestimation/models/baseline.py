f6c509a9f376a12ea648842d0c6a95c426902d8f
François LaBerge <fgrclaberge@gmail.com>
2022-08-03 14:18:25 -0400

    drop the batch remainders in the dataset

--- mlbpestimation/models/baseline.py
+++ mlbpestimation/models/baseline.py
@@ -6,7 +6,8 @@ from mlbpestimation.preprocessing.pipelines.windowpreprocessing import WindowPre
 def build_baseline_model(datasets, batch_size=20, frequency=500, window_size=8):
     input_shape = (window_size * frequency, 1)
     pipeline = WindowPreprocessing(frequency=frequency, window_size=window_size)
-    preprocessed_datasets = [pipeline.preprocess(dataset).batch(batch_size) for dataset in datasets]
+    preprocessed_datasets = [pipeline.preprocess(dataset).batch(batch_size, drop_remainder=True) for dataset in
+                             datasets]
     model = Sequential([
         Input(input_shape, batch_size),
         layers.Conv1D(64, 15, activation='relu'),
