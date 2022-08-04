919419a6f905d4c0767cd95ff944b52cc0152262
François LaBerge <fgrclaberge@gmail.com>
2022-08-04 14:26:44 -0400

    try to enable cuda for training in a container

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@
 # Graham GPU node: 32 cores, 128G ram, 2 GPUs (12G each)
 #SBATCH --account def-bentahar
 #SBATCH --gres=gpu:v100l:1               # Number of GPUs (per node)
-#SBATCH --cpus-per-task=8          # Number of cores (not cpus)
+#SBATCH --cpus-per-task=8          # Number of cores (not cpus)sa
 #SBATCH --mem=64G               # memory (per node)
 #SBATCH --time=0-02:00             # time (DD-HH:MM)
 
