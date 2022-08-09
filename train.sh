10d2ff8edad8d2e851f7e0d041ba04b4d1228446
François LaBerge <fgrclaberge@gmail.com>
2022-08-08 20:16:44 -0400

    revert to sbatch and increase training time

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:v100l:1               # Number of GPUs (per node)
 #SBATCH --cpus-per-task=8          # Number of cores (not cpus)sa
 #SBATCH --mem=64G               # memory (per node)
-#SBATCH --time=0-02:00             # time (DD-HH:MM)
+#SBATCH --time=0-05:00             # time (DD-HH:MM)
 
 # Setup and run task -------------
 module load apptainer/1.0 cuda/11.7
