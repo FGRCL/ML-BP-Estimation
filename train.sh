34e803767f86584a76d2edeacf91f6c4397ac7f5
François LaBerge <fgrclaberge@gmail.com>
2022-08-10 17:57:45 -0400

    precreate log file

--- train.sh
+++ train.sh
@@ -19,4 +19,5 @@ module load apptainer/1.0 cuda/11.7
 apptainer run --nv --env-file variables.env --env WANDB_RUN_NAME=$1 --bind /home/fgrcl/projects/def-bentahar/fgrcl/ML-BP-Estimation/data/mimic-IV:/mnt/mimic4 docker://fgrcl/ml-bp-estimation:$1
 EOT
 
-tail --pid=$! -F console.out
+touch console.out
+tail --pid=$! --follow console.out
