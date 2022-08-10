a4666c3f8a8aafe91e4559ec88a238720054eca0
François LaBerge <fgrclaberge@gmail.com>
2022-08-10 11:00:15 -0400

    reduce training time and run interactively

--- train.sh
+++ train.sh
@@ -3,10 +3,10 @@
 # Request resources --------------
 # Graham GPU node: 32 cores, 128G ram, 2 GPUs (12G each)
 #SBATCH --account def-bentahar
-#SBATCH --gres=gpu:v100l:1               # Number of GPUs (per node)
+##SBATCH --gres=gpu:v100l:1               # Number of GPUs (per node)
 #SBATCH --cpus-per-task=8          # Number of cores (not cpus)sa
 #SBATCH --mem=64G               # memory (per node)
-#SBATCH --time=0-05:00             # time (DD-HH:MM)
+#SBATCH --time=0-00:30             # time (DD-HH:MM)
 
 # Setup and run task -------------
 module load apptainer/1.0 cuda/11.7
