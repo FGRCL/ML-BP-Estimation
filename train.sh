3b3c5d4550dea9c05ebd8f3172705ef9457ec112
François LaBerge <fgrclaberge@gmail.com>
2023-03-27 17:10:54 -0400

    create training script without containers and add requirements.txt to gitignore

--- train.sh
+++ train.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-
-sbatch <<EOT
-#!/bin/bash
-
-# Request resources --------------
-# Graham GPU node: 32 cores, 128G ram, 2 GPUs (12G each)
 #SBATCH --account def-bentahar
-#SBATCH --job-name=$1
-#SBATCH --output=console.out
-#SBATCH --gres=gpu:1               # Number of GPUs (per node)
-#SBATCH --cpus-per-task=16          # Number of cores (not cpus)
-#SBATCH --mem=128G               # memory (per node)
-#SBATCH --time=2-00:00             # time (DD-HH:MM)
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=3  # Refer to cluster's documentation for the right CPU/GPU ratio
+#SBATCH --mem=64G          # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --time=0-03:00     # DD-HH:MM:SS
 
-# Setup and run task -------------
-module load apptainer/1.0 cuda/11.7
-apptainer run --nv --env-file variables.env --env WANDB_RUN_NAME=$1 --bind /home/fgrcl/projects/def-bentahar/fgrcl/ML-BP-Estimation/data/mimic-IV:/mnt/mimic4 docker://fgrcl/ml-bp-estimation:$1
-EOT
+module load python/3.8 cuda cudnn
+
+poetry export --format=requirements.txt > requirements.txt
+
+rm -rf venv
+virtualenv --no-download venv
+source venv/bin/activate
+pip install --no-index -r requirements.txt
+
+cp /home/fgrcl/projects/def-bentahar/fgrcl/ML-BP-Estimation/data/mimic-IV/physionet.org/files/mimic4wdb/0.1.0/waves "$SLURM_TMPDIR"
+export MIMIC_FILE_LOCATION="$SLURM_TMPDIR"
+
+cd $SOURCEDIR
+python -m mlbpestimation.train baseline_window_vitaldb
