ceaeba0d1f96b9c46e0a422d59ba0faf379f6284
François LaBerge <fgrclaberge@gmail.com>
2023-03-28 09:43:41 -0400

    Change memory amount

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 #SBATCH --account def-bentahar
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=3  # Refer to cluster's documentation for the right CPU/GPU ratio
-#SBATCH --mem=64G          # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --mem=32G          # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 #SBATCH --time=0-03:00     # DD-HH:MM:SS
 
 module load python/3.8 cuda cudnn
