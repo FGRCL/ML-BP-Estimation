b4dbe9dbc6ead9a36f2500bb18283fb49ce72b3c
François LaBerge <fgrclaberge@gmail.com>
2023-03-28 16:13:53 -0400

    increase amount of resources

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account def-bentahar
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
-#SBATCH --cpus-per-task=3  # Refer to cluster's documentation for the right CPU/GPU ratio
-#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=0-03:00     # DD-HH:MM:SS
+#SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
+#SBATCH --mem=128G       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --time=2-00:00     # DD-HH:MM:SS
 
 module load python/3.8 cuda cudnn
 
