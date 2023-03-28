29cd3787f64a997dcb31dc95dd5c8fc04d865b23
François LaBerge <fgrclaberge@gmail.com>
2023-03-28 13:36:48 -0400

    remove sourcedir

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 #SBATCH --account def-bentahar
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=3  # Refer to cluster's documentation for the right CPU/GPU ratio
-#SBATCH --mem=32G          # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 #SBATCH --time=0-03:00     # DD-HH:MM:SS
 
 module load python/3.8 cuda cudnn
@@ -16,5 +16,4 @@ pip install -r requirements.txt
 cp -r /home/fgrcl/projects/def-bentahar/fgrcl/ML-BP-Estimation/data/mimic-IV/physionet.org/files/mimic4wdb/0.1.0/waves "$SLURM_TMPDIR"
 export MIMIC_FILE_LOCATION="$SLURM_TMPDIR"
 
-cd $SOURCEDIR
 python -m mlbpestimation.train baseline_window_vitaldb
