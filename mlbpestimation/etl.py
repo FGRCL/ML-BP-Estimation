d3b5d80d91b1737853bc723c8a9435ee3808ccca
François LaBerge <fgrclaberge@gmail.com>
2023-03-31 15:42:08 -0400

    remove the profile because it causes memory leaks

--- mlbpestimation/etl.py
+++ mlbpestimation/etl.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-from tensorflow.python.profiler.profiler_v2 import Profile
-
 from mlbpestimation.data.datasource.mimic4.mimicdatabase import MimicDatabase
 from mlbpestimation.data.datasource.vitaldb.vitaldatabase import VitalDatabase
 from mlbpestimation.data.featureset import FeatureSet
@@ -20,12 +18,12 @@ preprocessing_pipelines = {
 
 
 def main():
-    with Profile('logdir'):
-        fs = FeatureSet(MimicDatabase(), WindowPreprocessing(63)).build_featuresets()
-        try:
-            fs.save(Path(__file__).parent.parent / 'data' / 'example-test')
-        except(RuntimeWarning, UserWarning):
-            pass
+    # with Profile('logdir'):
+    fs = FeatureSet(MimicDatabase(0.05), WindowPreprocessing(63)).build_featuresets()
+    try:
+        fs.save(Path(__file__).parent.parent / 'data' / 'example-test')
+    except(RuntimeWarning, UserWarning):
+        pass
 
 
 if __name__ == '__main__':
