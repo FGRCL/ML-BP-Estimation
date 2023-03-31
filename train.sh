d19006decb64ed45e56fc9d688c150cde18f5c01
François LaBerge <fgrclaberge@gmail.com>
2023-03-31 10:51:47 -0400

    change resource request for the training script

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account def-bentahar
-#SBATCH --gres=gpu:1       # Request GPU "generic resources"
-#SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
-#SBATCH --mem=128G       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=2-00:00     # DD-HH:MM:SS
+#SBATCH --gres=gpu:2       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=1  # Refer to cluster's documentation for the right CPU/GPU ratio
+#SBATCH --mem=8G       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --time=4-00:00     # DD-HH:MM:SS
 
 module load python/3.8 cuda cudnn
 
