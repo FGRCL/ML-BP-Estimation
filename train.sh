6418658e7e369a3bfd1821dff472b24364d3e79b
François LaBerge <fgrclaberge@gmail.com>
2023-04-03 15:09:47 -0400

    remove training time and fix required gpu count

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account def-bentahar
-#SBATCH --gres=gpu:21       # Request GPU "generic resources"
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=1  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=4-00:00     # DD-HH:MM:SS
+#SBATCH --time=0-01:00     # DD-HH:MM:SS
 
 module load python/3.8 cuda cudnn
 
