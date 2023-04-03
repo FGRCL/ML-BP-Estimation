824ed52aac23a06ecefd7a6e6b9556831f50b00b
François LaBerge <fgrclaberge@gmail.com>
2023-04-03 17:42:06 -0400

    make MimicDataset and VitalDataset loader more readable

--- mlbpestimation/data/vitaldb/vitaldatasetloader.py
+++ mlbpestimation/data/vitaldb/vitaldatasetloader.py
@@ -1,7 +1,9 @@
 from numpy import arange, split
+from numpy.random import choice, seed, shuffle
 from tensorflow import TensorSpec, float32
 from tensorflow.python.data import Dataset
 
+from mlbpestimation.configuration import configuration
 from mlbpestimation.data.datasetloader import DatasetLoader
 from mlbpestimation.data.splitdataset import SplitDataset
 from mlbpestimation.data.vitaldb.casegenerator import MAX_VITAL_DB_CASE, MIN_VITAL_DB_CASE, VitalDBGenerator, \
@@ -10,6 +12,8 @@ from mlbpestimation.data.vitaldb.fetchingstrategy.DatasetApi import DatasetApi
 
 
 class VitalDatasetLoader(DatasetLoader):
+    def __init__(self, subsample: float = 1.0):
+        self.subsample = subsample
 
     def load_datasets(self) -> SplitDataset:
         options = VitalFileOptions(
@@ -17,9 +21,9 @@ class VitalDatasetLoader(DatasetLoader):
             1 / 500
         )
 
-        case_ids = arange(MIN_VITAL_DB_CASE, MAX_VITAL_DB_CASE + 1)
-        nb_cases = len(case_ids)
-        case_id_splits = split(case_ids, [int(nb_cases * 0.70), int(nb_cases * 0.85)])
+        case_ids = self._get_random_case_id_list()
+        case_ids = self._subsample_items(case_ids)
+        case_id_splits = self._make_splits(case_ids)
 
         datasets = []
         for case_split in case_id_splits:
@@ -33,3 +37,18 @@ class VitalDatasetLoader(DatasetLoader):
             )
 
         return SplitDataset(*datasets)
+
+    def _get_random_case_id_list(self):
+        case_ids = arange(MIN_VITAL_DB_CASE, MAX_VITAL_DB_CASE + 1)
+        seed(configuration['random_seed'])
+        shuffle(case_ids)
+        return case_ids
+
+    def _subsample_items(self, case_ids):
+        nb_cases = len(case_ids)
+        subsample_size = int(nb_cases * self.subsample)
+        return choice(nb_cases, subsample_size, False)
+
+    def _make_splits(self, case_ids):
+        nb_cases = len(case_ids)
+        return split(case_ids, [int(nb_cases * 0.70), int(nb_cases * 0.85)])
