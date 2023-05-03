6bb30fc20e8dd9a10703ebc77e51e51930586723
François LaBerge <fgrclaberge@gmail.com>
2023-05-03 15:27:20 -0400

    remove GPU requirement

--- train.sh
+++ train.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH --account def-bentahar
-#SBATCH --gres=gpu:2       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=2  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 #SBATCH --time=5-00:00     # DD-HH:MM:SS
