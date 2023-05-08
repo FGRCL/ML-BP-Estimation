6bde7acd8c8f800ca28ec2a8d901e4fb10d5d41d
François LaBerge <fgrclaberge@gmail.com>
2023-05-08 14:55:02 -0400

    add a train config

--- mlbpestimation/train.py
+++ mlbpestimation/train.py
@@ -1,9 +1,23 @@
 import argparse
+from dataclasses import dataclass
 
-from mlbpestimation.hypothesis import hypotheses_repository
+from hydra import main
+from hydra.core.config_store import ConfigStore
+from omegaconf import DictConfig
+
+from mlbpestimation.hypothesis import Hypothesis, hypotheses_repository
 
 
-def main():
+@dataclass
+class TrainConfig:
+    hypothesis: Hypothesis
+
+
+ConfigStore.instance()
+
+
+@main(version_base=None, config_path='', config_name='')
+def main(configuration: DictConfig):
     parser = argparse.ArgumentParser()
     parser.add_argument('hypothesis', choices=hypotheses_repository.keys(), nargs=1)
     args = parser.parse_args()
