046e12b05efac8e222954ec540949d75f89ca46f
François LaBerge <fgrclaberge@gmail.com>
2023-05-12 16:09:03 -0400

    add a local environment config

--- mlbpestimation/train.py
+++ mlbpestimation/train.py
@@ -5,6 +5,7 @@ from omegaconf import DictConfig
 
 @main(version_base=None, config_path='conf', config_name='train_configuration')
 def main(configuration: DictConfig):
+    print(configuration.environment.wandb.api_key)
     hypothesis = instantiate(configuration.hypothesis)
     hypothesis.train()
 
