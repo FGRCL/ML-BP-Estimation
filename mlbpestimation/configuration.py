fcfcb9c7451dcde12c4d2e3e9296b4479f9bd2bc
François LaBerge <fgrclaberge@gmail.com>
2023-05-15 13:50:46 -0400

    remove old config object and rename the config directory

--- mlbpestimation/configuration.py
+++ mlbpestimation/configuration.py
@@ -3,5 +3,5 @@ from hydra import compose, initialize
 
 load_dotenv()
 
-initialize(version_base=None, config_path='conf')
+initialize(version_base=None, config_path='configuration')
 configuration = compose(config_name='train_configuration')
