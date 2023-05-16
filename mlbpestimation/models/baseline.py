c9c5c755f98b22d95f31e807ac81159c62e4d101
François LaBerge <35940434+FGRCL@users.noreply.github.com>
2023-05-16 15:49:46 -0400

    Hydra (#24)
    * increase the network depth and decrease the learning rate
    
    * add a train config
    
    * add a working configuration
    
    * add configs for models
    
    * add configs for dataset loaders
    
    * remove useless parameters in the Baseline model
    
    * remove hypothesis repository
    
    * add a local environment config
    
    * add a local environment config
    
    * add a config for wandb and directories and change the code accordingly
    
    * don't print the api key
    
    * remove old config object and rename the config directory
    
    * remove environments template
    
    * remove some environment variable setup in train.sh
    
    * Add structured config for type validation
    
    * remove the global configuration imports
    
    * remove the global configuration imports and add a config for etl
    
    * add the uci dataset configs
    
    * add the uci dataset configs
    
    * change the train script

--- mlbpestimation/models/baseline.py
+++ mlbpestimation/models/baseline.py
@@ -1,15 +1,12 @@
 import tensorflow
-from tensorflow.python.keras.engine.input_layer import InputLayer
 from tensorflow.python.keras.layers import Conv1D, Dense, Dropout, LSTM, MaxPooling1D
 from tensorflow.python.keras.models import Model, Sequential
 
 
 class Baseline(Model):
-    def __init__(self, frequency=500, window_size=8, batch_size=20):
+    def __init__(self):
         super().__init__()
-        input_shape = (window_size * frequency, 1)
         self.layer = Sequential([
-            InputLayer(input_shape, batch_size),
             Conv1D(64, 15, activation='relu'),
             tensorflow.keras.layers.BatchNormalization(),
             MaxPooling1D(4),
