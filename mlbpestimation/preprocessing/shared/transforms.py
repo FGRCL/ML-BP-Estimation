2cb42aef5530880fc1822d44d738c3ee631b2e76
François LaBerge <35940434+FGRCL@users.noreply.github.com>
2023-05-17 09:17:31 -0400

    Heartbeat preprocessing (#25)
    * Mostly fix heartbeat preprocessing
    
    * Make the hearbeat preprocessing output beats
    
    * remove filtering extra peaks
    
    * change filtering to scipy
    
    * Set the sqi values
    
    * return nothing when the heartbeat detection can't find a beat
    
    * save the preprocessed mimic dataset
    
    * add uci heartbeat configs
    
    * add uci heartbeat configs

--- mlbpestimation/preprocessing/shared/transforms.py
+++ mlbpestimation/preprocessing/shared/transforms.py
@@ -1,8 +1,9 @@
 from typing import Any, Tuple, Union
 
 import tensorflow as tf
-from heartpy import filter_signal
 from numpy import asarray, float32, ndarray
+from scipy.signal import butter, sosfilt
+from scipy.stats import skew
 from tensorflow import DType, Tensor, cast, reduce_max, reduce_min, reshape
 from tensorflow.python.data import Dataset
 
@@ -29,11 +30,11 @@ class SignalFilter(NumpyTransformOperation):
         self.lowpass_cutoff = lowpass_cutoff
         self.sample_rate = sample_rate
 
-    def transform(self, track: ndarray, y: ndarray = None) -> Any:
-        track_lowpass = asarray(filter_signal(data=track, cutoff=self.lowpass_cutoff, sample_rate=self.sample_rate,
-                                              filtertype='lowpass'), dtype=float32)
-        track_bandpass = asarray(filter_signal(data=track, cutoff=self.bandpass_cutoff, sample_rate=self.sample_rate,
-                                               filtertype='bandpass'), dtype=float32)
+    def transform(self, signal: ndarray, y: ndarray = None) -> Any:
+        lowpass_filter = butter(2, self.lowpass_cutoff, 'lowpass', output='sos', fs=self.sample_rate)
+        bandpass_filter = butter(2, self.bandpass_cutoff, 'bandpass', output='sos', fs=self.sample_rate)
+        track_lowpass = asarray(sosfilt(lowpass_filter, signal), dtype=float32)
+        track_bandpass = asarray(sosfilt(bandpass_filter, signal), dtype=float32)
 
         return [track_lowpass, track_bandpass]
 
@@ -70,3 +71,17 @@ class Cast(TransformOperation):
 
     def transform(self, x: Tensor, y: Tensor = None) -> Any:
         return cast(x, self.dtype), cast(y, self.dtype)
+
+
+class ComputeSqi(NumpyTransformOperation):
+    def __init__(self, out_type: Union[DType, Tuple[DType, ...]]):
+        super().__init__(out_type)
+
+    def transform(self, window_lowpass: ndarray, window_bandpass: ndarray) -> Any:
+        sqi = skew(window_bandpass)
+        return window_lowpass, window_bandpass, asarray(sqi, dtype=float32)
+
+
+class RemoveSqi(TransformOperation):
+    def transform(self, lowpass_window: ndarray, bandpass_window: ndarray, sqi: ndarray) -> Any:
+        return lowpass_window, bandpass_window
