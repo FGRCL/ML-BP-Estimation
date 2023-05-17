9d18ba1824598963906f9ae504c3418b818ab197
François LaBerge <fgrclaberge@gmail.com>
2023-05-17 10:58:47 -0400

    and configuration of hyperparameters

--- mlbpestimation/hypothesis.py
+++ mlbpestimation/hypothesis.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 
 import wandb
+from omegaconf import DictConfig
 from tensorflow.python.data import AUTOTUNE
-from tensorflow.python.keras.losses import MeanSquaredError
 from tensorflow.python.keras.metrics import MeanAbsoluteError
 from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.optimizer_v2.adam import Adam
 from wandb.integration.keras import WandbMetricsLogger, WandbModelCheckpoint
 
 from mlbpestimation.data.datasetloader import DatasetLoader
@@ -13,30 +12,29 @@ from mlbpestimation.metrics.standardeviation import AbsoluteError, StandardDevia
 
 
 class Hypothesis:
-    def __init__(self, dataset_loader: DatasetLoader, model: Model, output_directory: str):
+    def __init__(self, dataset_loader: DatasetLoader, model: Model, output_directory: str, optimization: DictConfig):
         self.dataset_loader = dataset_loader
         self.model = model
+        self.optimization = optimization
         self.output_directory = str(output_directory)
-
-    def train(self):
-        datasets = self.dataset_loader.load_datasets()
-        train = datasets.train \
-            .batch(20, drop_remainder=True, num_parallel_calls=AUTOTUNE) \
-            .prefetch(AUTOTUNE)
-        validation = datasets.validation \
-            .batch(20, drop_remainder=True, num_parallel_calls=AUTOTUNE)
-
-        loss = MeanSquaredError()
-        metrics = [
+        self.metrics = [
             MeanAbsoluteError(),
             StandardDeviation(AbsoluteError())
         ]
-        self.model.compile(Adam(1e-6), loss=loss, metrics=metrics)
 
-        self.model.fit(train,
-                       epochs=100,
-                       callbacks=[*self._get_wandb_callbacks()],
-                       validation_data=validation)
+    def train(self):
+        train, validation = self.setup_train_val()
+        self.model.compile(self.optimization.optimizer, loss=self.optimization.loss, metrics=self.metrics)
+        self.model.fit(train, epochs=self.optimization.epoch, callbacks=[*self._get_wandb_callbacks()], validation_data=validation)
+
+    def setup_train_val(self):
+        datasets = self.dataset_loader.load_datasets()
+        train = datasets.train \
+            .batch(self.optimization.batch_size, drop_remainder=True, num_parallel_calls=AUTOTUNE) \
+            .prefetch(AUTOTUNE)
+        validation = datasets.validation \
+            .batch(self.optimization.batch_size, drop_remainder=True, num_parallel_calls=AUTOTUNE)
+        return train, validation
 
     def _get_wandb_callbacks(self):
         return [
