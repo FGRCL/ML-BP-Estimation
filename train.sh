5e2c45d8314ae6b42c6450e13fe7dce57268ef42
François LaBerge <fgrclaberge@gmail.com>
2023-05-18 14:45:01 -0400

    increase the time and change wandb to an online run

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ sbatch <<EOT
 #SBATCH --account def-bentahar
 #SBATCH --cpus-per-task=2  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=0-00:30     # DD-HH:MM:SS
+#SBATCH --time=10-00:00     # DD-HH:MM:SS
 
 module load python/3.8 cuda cudnn
 
@@ -14,5 +14,5 @@ pip install -r requirements.txt
 
 export HYDRA_FULL_ERROR=1
 export WANDB__SERVICE_WAIT=300
-python -m mlbpestimation.train directories=cc wandb=dev $@
+python -m mlbpestimation.train directories=cc wandb=online $@
 EOT
