0d2cb8265fb62b92d8928a895ce33772256c840f
François LaBerge <fgrclaberge@gmail.com>
2023-05-18 13:06:03 -0400

    reduce training time, remove gpu and start wandb in dev mode

--- train.sh
+++ train.sh
@@ -2,10 +2,9 @@
 sbatch <<EOT
 #!/bin/bash
 #SBATCH --account def-bentahar
-#SBATCH --gres=gpu:2       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=2  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=5-00:00     # DD-HH:MM:SS
+#SBATCH --time=0-00:10     # DD-HH:MM:SS
 
 module load python/3.8 cuda cudnn
 
@@ -15,5 +14,5 @@ pip install -r requirements.txt
 
 cp -r /home/fgrcl/projects/def-bentahar/fgrcl/ML-BP-Estimation/data/. "$SLURM_TMPDIR"
 
-python -m mlbpestimation.train directories=cc wandb=online "$a"
+python -m mlbpestimation.train directories=cc wandb=dev "$a"
 EOT
