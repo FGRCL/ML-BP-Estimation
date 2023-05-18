da996786503d6e442048403ec177142c8a289990
François LaBerge <fgrclaberge@gmail.com>
2023-05-18 14:34:26 -0400

    increase wandb wait time

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ sbatch <<EOT
 #SBATCH --account def-bentahar
 #SBATCH --cpus-per-task=2  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=0-00:10     # DD-HH:MM:SS
+#SBATCH --time=0-00:30     # DD-HH:MM:SS
 
 module load python/3.8 cuda cudnn
 
@@ -13,5 +13,6 @@ pip install --no-index --upgrade pip
 pip install -r requirements.txt
 
 export HYDRA_FULL_ERROR=1
+export WANDB__SERVICE_WAIT=300
 python -m mlbpestimation.train directories=cc wandb=dev $@
 EOT
