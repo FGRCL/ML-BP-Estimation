93aa42e76c59077c6a41493088caf824f4e14dd6
François LaBerge <fgrclaberge@gmail.com>
2023-05-18 13:59:01 -0400

    print all the arguments

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sbatch <<EOT
-#!/bin/bash
+#!/bin/sh
 #SBATCH --account def-bentahar
 #SBATCH --cpus-per-task=2  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=0-00:10     # DD-HH:MM:SS
+#SBATCH --time=0-00:5     # DD-HH:MM:SS
 
 module load python/3.8 cuda cudnn
 
@@ -13,6 +13,6 @@ pip install --no-index --upgrade pip
 pip install -r requirements.txt
 
 export HYDRA_FULL_ERROR=1
-echo "$1"
+echo $a
 python -m mlbpestimation.train directories=cc wandb=dev "$a"
 EOT
