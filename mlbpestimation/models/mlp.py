b2ba6a28055518ea04fbe22061113da5bdb43149
François LaBerge <fgrclaberge@gmail.com>
2023-05-19 17:17:03 -0400

    reshape the input before using it

--- mlbpestimation/models/mlp.py
+++ mlbpestimation/models/mlp.py
@@ -1,12 +1,17 @@
-from keras.layers import Dense
+from tensorflow import reshape
 from tensorflow.python.keras import Model, Sequential
+from tensorflow.python.keras.engine.input_layer import InputLayer
+from tensorflow.python.keras.layers import Dense
 
 
 class MLP(Model):
-    def __init__(self, neurons):
-        self.layers = Sequential()
+    def __init__(self, neurons, *args, **kwargs):  # TODO activations
+        super().__init__(*args, **kwargs)
+        self._layers = Sequential()
+        self._layers.add(InputLayer(504))
         for neuron_count in neurons:
-            self.layers.append(Dense(neuron_count))
+            self._layers.add(Dense(neuron_count, use_bias=False))
 
     def call(self, inputs, training=None, mask=None):
-        return self.layers(inputs, training, mask)
+        inputs_reshaped = reshape(inputs, [*inputs.shape[:-1]])
+        return self._layers(inputs_reshaped, training, mask)
