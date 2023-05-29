024d85996ade31b0ca364c0e6d6cc97b95d2bba2
François LaBerge <fgrclaberge@gmail.com>
2023-05-29 13:45:25 -0400

    add sbatch scripts to run sweeps

---
+++ sweep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sbatch <<EOT
+#!/bin/sh
+#SBATCH --account def-bentahar
+#SBATCH --cpus-per-task=2  # Refer to cluster's documentation for the right CPU/GPU ratio
+#SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --time=10-00:00     # DD-HH:MM:SS
+#SBATCH --array=1-$2
+
+module load python/3.8 cuda cudnn
+
+source venv/bin/activate
+pip install --no-index --upgrade pip
+pip install -r requirements.txt
+
+export HYDRA_FULL_ERROR=1
+export WANDB__SERVICE_WAIT=300
+wandb agent --count 1 $1
+python -m mlbpestimation.train directories=cc wandb=online $@
+EOT
