7fa7d99cb67e5bbec5dc7efbb288a7abd2d96ff7
François LaBerge <35940434+FGRCL@users.noreply.github.com>
2023-06-01 09:53:23 -0400

    decrease the training time (#51)

--- sweep.sh
+++ sweep.sh
@@ -4,7 +4,7 @@ sbatch <<EOT
 #SBATCH --account def-bentahar
 #SBATCH --cpus-per-task=2  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=1-00:00     # DD-HH:MM:SS
+#SBATCH --time=0-06:00     # DD-HH:MM:SS
 #SBATCH --array=1-$2
 
 module load python/3.8 cuda cudnn
