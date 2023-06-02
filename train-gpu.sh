aad44776301633ac84883d402c87e994743c713e
François LaBerge <francois@pop-os.localdomain>
2023-06-02 10:04:51 -0400

    add use absolute path for slurm logs

--- train-gpu.sh
+++ train-gpu.sh
@@ -6,7 +6,7 @@ sbatch <<EOT
 #SBATCH --cpus-per-task=2  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 #SBATCH --time=10-00:00     # DD-HH:MM:SS
-#SBATCH --output=~/scratch/job-logs/slurm-%A_%a.out
+#SBATCH --output=/home/fgrcl/scratch/job-logs/slurm-%A_%a.out
 
 module load python/3.8 cuda cudnn
 
