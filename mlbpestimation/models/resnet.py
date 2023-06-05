4c55ac268ccf0a8f7ab0af9296dbb5507a9b4153
François LaBerge <35940434+FGRCL@users.noreply.github.com>
2023-06-05 15:59:28 -0400

    Resnet sweep (#55)
    * change the parameters to make resnet easier to sweep
    
    * fix some class properties

--- mlbpestimation/models/resnet.py
+++ mlbpestimation/models/resnet.py
@@ -2,42 +2,88 @@ from keras import Sequential
 from keras.engine.base_layer import Layer
 from keras.layers import Add, BatchNormalization, Conv1D, Dense, Dropout, Flatten, ReLU
 from keras.regularizers import L2
-from omegaconf import ListConfig
+from numpy import arange, concatenate, full, log2
 
 from mlbpestimation.models.basemodel import BloodPressureModel
 
 
 class ResNet(BloodPressureModel):
-    def __init__(self, n_residual_blocks: ListConfig, n_filters: ListConfig):
+    def __init__(self,
+                 start_filters: int,
+                 max_filters: int,
+                 first_octave_modules: int,
+                 second_octave_modules: int,
+                 third_octave_modules: int,
+                 fourth_octave_modules: int,
+                 regressor_layers: int,
+                 regressor_units: int,
+                 regressor_dropout: float,
+                 regressor_activation: str,
+                 l2_lambda: float,
+                 output_units: int):
         super().__init__()
-        self.n_residual_blocks = list(n_residual_blocks)
-        self.n_filters = list(n_filters)
+        self.start_filters = start_filters
+        self.max_filters = max_filters
+        self.first_octave_modules = first_octave_modules
+        self.second_octave_modules = second_octave_modules
+        self.third_octave_modules = third_octave_modules
+        self.fourth_octave_modules = fourth_octave_modules
+        self.regressor_layers = regressor_layers
+        self.regressor_units = regressor_units
+        self.regressor_dropout = regressor_dropout
+        self.regressor_activation = regressor_activation
+        self.l2_lambda = l2_lambda
+        self.output_units = output_units
 
-        self.octaves = Sequential()
-        for n_residual_block, n_filter in zip(self.n_residual_blocks, self.n_filters):
-            self.octaves.add(ResidualOctave(n_filter, n_residual_block))
-        self.regressor = Sequential([
-            Flatten(),
-            Dense(32, 'relu', kernel_regularizer=L2(.001)),
-            Dropout(0.25),
-            Dense(32, 'relu', kernel_regularizer=L2(.001)),
-            Dropout(0.25),
-            Dense(2, 'relu')
-        ])
+        self._octaves = Sequential()
+        filters = self._compute_filters(start_filters, max_filters)
+        octave_modules = [first_octave_modules, second_octave_modules, third_octave_modules, fourth_octave_modules]
+        for n_residual_block, n_filter in zip(octave_modules, filters):
+            self._octaves.add(ResidualOctave(n_filter, n_residual_block))
+
+        self._flatten = Flatten()
+        self._regressor = Sequential()
+        for _ in range(regressor_layers):
+            self._regressor.add(Dense(regressor_units, regressor_activation, kernel_regularizer=L2(l2_lambda)))
+            self._regressor.add(Dropout(regressor_dropout))
+        self._output = Dense(output_units)
 
     def call(self, inputs, training=None, mask=None):
-        x = self.octaves(inputs)
-        x = self.regressor(x)
-        return x
+        x = self._octaves(inputs)
+        x = self._flatten(x)
+        x = self._regressor(x)
+        return self._output(x)
 
     def set_input_shape(self, dataset_spec):
         pass
 
     def get_config(self):
         return {
-            'n_residual_blocks': self.n_residual_blocks,
-            'n_filters': self.n_filters
+            'start_filters': self.start_filters,
+            'max_filters': self.max_filters,
+            'first_octave_modules': self.first_octave_modules,
+            'second_octave_modules': self.second_octave_modules,
+            'third_octave_modules': self.third_octave_modules,
+            'fourth_octave_modules': self.fourth_octave_modules,
+            'regressor_layers': self.regressor_layers,
+            'regressor_units': self.regressor_units,
+            'regressor_dropout': self.regressor_dropout,
+            'regressor_activation': self.regressor_activation,
+            'l2_lambda': self.l2_lambda,
+            'output_units': self.output_units,
         }
+
+    @staticmethod
+    def _compute_filters(start_filter, max_filter):
+        filters = arange(log2(start_filter), log2(max_filter) + 1, dtype=int)
+        n_leftover_filters = len(filters) - 4
+        if n_leftover_filters > 0:
+            leftover_filters = full(n_leftover_filters, log2(max_filter), dtype=int)
+            filters = concatenate((filters, leftover_filters))
+        if n_leftover_filters < 0:
+            filters = filters[:4]
+
+        return 2 ** filters.astype(int)
 
 
 class ResidualOctave(Layer):
