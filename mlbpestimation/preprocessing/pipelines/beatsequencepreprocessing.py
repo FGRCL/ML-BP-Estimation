ba4b326f28627ba52f31a3f7342216f75c135b94
François LaBerge <fgrclaberge@gmail.com>
2023-06-07 13:48:04 -0400

    keep only the last pressure

--- mlbpestimation/preprocessing/pipelines/beatsequencepreprocessing.py
+++ mlbpestimation/preprocessing/pipelines/beatsequencepreprocessing.py
@@ -1,8 +1,8 @@
 from typing import Tuple
 
-from tensorflow import float32
+from tensorflow import Tensor, float32
 
-from mlbpestimation.preprocessing.base import DatasetPreprocessingPipeline
+from mlbpestimation.preprocessing.base import DatasetPreprocessingPipeline, TransformOperation
 from mlbpestimation.preprocessing.pipelines.heartbeatpreprocessing import SplitHeartbeats
 from mlbpestimation.preprocessing.shared.filters import FilterPressureWithinBounds, HasData
 from mlbpestimation.preprocessing.shared.pipelines import SqiFiltering
@@ -25,6 +25,12 @@ class BeatSequencePreprocessing(DatasetPreprocessingPipeline):
             AddBloodPressureOutput(1),
             RemoveLowpassTrack(),
             FilterPressureWithinBounds(min_pressure, max_pressure),
+            RemovePressures(),
             StandardizeArray(1),
             SetTensorShape([sequence_steps, beat_length])
         ])
+
+
+class RemovePressures(TransformOperation):
+    def transform(self, beat_sequence: Tensor, pressures: Tensor) -> (Tensor, Tensor):
+        return beat_sequence, pressures[:, -1]
