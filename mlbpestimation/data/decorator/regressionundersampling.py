cc952e8d0f412aae77fdef9e836c6089a8183539
François LaBerge <fgrclaberge@gmail.com>
2023-08-02 09:29:18 -0400

    rewrite to decorator for readability

--- mlbpestimation/data/decorator/regressionundersampling.py
+++ mlbpestimation/data/decorator/regressionundersampling.py
@@ -12,21 +12,13 @@ class RegressionUndersampling(DatasetLoader):
         self.sampling_ratio = sampling_ratio
 
     def load_datasets(self) -> SplitDataset:
-        # TODO needs rewrite
         train, validation, test = self.dataset_loader.load_datasets()
         majority = train.filter(self._pressures_within_bounds)
-        minority = train.filter(lambda input_signal, pressures: not self._pressures_within_bounds(input_signal, pressures))
+        minority = train.filter(self._pressure_out_of_bounds)
 
-        minority_n_samples = int(minority.reduce(0, lambda x, _: x + 1))
-        majority_n_resampled = int(float(minority_n_samples) / self.sampling_ratio)
-
-        majority_n_samples = int(majority.reduce(0, lambda x, _: x + 1))
-        majority = majority.shuffle(majority_n_samples)
-        majority_resampled = majority.take(majority_n_resampled)
-
-        train_resampled = majority_resampled.concatenate(minority)
-        train_n_resampled = int(train_resampled.reduce(0, lambda x, _: x + 1))
-        train_resampled = train_resampled.shuffle(train_n_resampled)
+        n_resampled = self._compute_n_resampled(minority)
+        majority_resampled = self._resample_majority(majority, n_resampled)
+        train_resampled = self._merge(majority_resampled, minority)
 
         return SplitDataset(train_resampled, validation, test)
 
@@ -34,3 +26,27 @@ class RegressionUndersampling(DatasetLoader):
         sbp = pressures[0]
         dbp = pressures[1]
         return (self.dbp_threshold < dbp) & (sbp < self.sbp_threshold)
+
+    def _pressure_out_of_bounds(self, input_signal: Tensor, pressures: Tensor) -> bool:
+        return not self._pressures_within_bounds(input_signal, pressures)
+
+    def _compute_n_resampled(self, minority):
+        n_minority = self._count_dataset(minority)
+        n_resampled = int(float(n_minority) / self.sampling_ratio)
+        return n_resampled
+
+    def _resample_majority(self, majority, n_resampled):
+        n_majority = self._count_dataset(majority)
+        majority = majority.shuffle(n_majority)
+        majority_resampled = majority.take(n_resampled)
+        return majority_resampled
+
+    def _merge(self, majority_resampled, minority):
+        train_resampled = majority_resampled.concatenate(minority)
+        train_n_resampled = self._count_dataset(train_resampled)
+        train_resampled = train_resampled.shuffle(train_n_resampled)
+        return train_resampled
+
+    @staticmethod
+    def _count_dataset(dataset):
+        return int(dataset.reduce(0, lambda x, _: x + 1))
