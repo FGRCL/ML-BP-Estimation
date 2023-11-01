b8cd4c7843e9fd46fe5a6b832010fcb9f8f51cb0
François LaBerge <fgrclaberge@gmail.com>
2023-11-01 13:24:52 -0400

    use autocorellation to filter more beats

--- mlbpestimation/preprocessing/pipelines/beatseries.py
+++ mlbpestimation/preprocessing/pipelines/beatseries.py
@@ -1,18 +1,17 @@
 from itertools import compress
 from typing import Tuple, Union
 
+import numpy
 from neurokit2 import ppg_clean, ppg_findpeaks
-from numpy import argmin, empty, float32, percentile, where, zeros
+from numpy import arange, argmin, concatenate, empty, float32, newaxis, percentile, reshape, where, zeros
 from scipy.ndimage import gaussian_filter1d
-from scipy.signal import resample
+from scipy.signal import correlate, resample
 from tensorflow import DType, Tensor
-from tensorflow.python.ops.ragged.ragged_concat_ops import stack
 
-from mlbpestimation.preprocessing.base import DatasetPreprocessingPipeline, Prefetch, PrintShape, PythonFunctionTransformOperation, Shuffle
-from mlbpestimation.preprocessing.pipelines.beatsequencepreprocessing import FilterBeats
+from mlbpestimation.preprocessing.base import DatasetPreprocessingPipeline, NumpyTransformOperation, Prefetch, Print, PrintShape
 from mlbpestimation.preprocessing.shared.filters import FilterSqi, HasData
 from mlbpestimation.preprocessing.shared.pipelines import FilterHasSignal
-from mlbpestimation.preprocessing.shared.transforms import AddBloodPressureSeries, AdjustPhaseLag, EnsureShape, FilterPressureSeriesWithinBounds, FlattenDataset, Reshape, SignalFilter, SlidingWindow, StandardScaling, Subsample
+from mlbpestimation.preprocessing.shared.transforms import AddBloodPressureSeries, AdjustPhaseLag, EnsureShape, FilterPressureSeriesWithinBounds, FlattenDataset, Reshape, SignalFilter, StandardScaling, Subsample
 
 
 class BeatSeriesPreprocessing(DatasetPreprocessingPipeline):
@@ -34,32 +33,34 @@ class BeatSeriesPreprocessing(DatasetPreprocessingPipeline):
             FilterHasSignal(),
             AdjustPhaseLag((float32, float32)),
             SignalFilter((float32, float32), frequency, lowpass_cutoff, bandpass_cutoff, bandpass_input),
-            SegmentHeartbeats((float32, float32), frequency, beat_length, 99.9),
-            FlattenDataset(),
-            PrintShape(),
-            FilterBeats(sequence_steps),
-            SlidingWindow(sequence_steps, sequence_stride),
-            Subsample((float32, float32), random_seed, subsample),
+            SequenceIntoHeartbeats((float32, float32), frequency, beat_length, sequence_steps, sequence_stride, 99.9),
             HasData(),
             FilterSqi((float32, float32), 0.5, 2),
             HasData(),
             AddBloodPressureSeries(),
             EnsureShape([None, sequence_steps, beat_length], [None, sequence_steps, 2]),
             FilterPressureSeriesWithinBounds(min_pressure, max_pressure),
+            HasData(),
             StandardScaling(axis=scaling_axis),
             Reshape([-1, sequence_steps, beat_length], [-1, sequence_steps, 2]),
+            PrintShape(),
+            Subsample((float32, float32), random_seed, subsample),
+            PrintShape(),
             FlattenDataset(),
-            Shuffle(),
+            Print("Done preprocessing"),
+            # Shuffle(),
             Prefetch(),
         ])
 
 
-class SegmentHeartbeats(PythonFunctionTransformOperation):
-    def __init__(self, out_type: Union[DType, Tuple[DType, ...]], frequency, beat_length, percentile):
+class SequenceIntoHeartbeats(NumpyTransformOperation):
+    def __init__(self, out_type: Union[DType, Tuple[DType, ...]], frequency, beat_length, width: int, shift: int, percentile):
         super().__init__(out_type)
         self.beat_length = beat_length
         self.frequency = frequency
         self.percentile = percentile
+        self.width = width
+        self.shift = shift
 
     def transform(self, lowpass_signal: Tensor, bandpass_signal: Tensor = None):
         try:
@@ -71,18 +72,34 @@ class SegmentHeartbeats(PythonFunctionTransformOperation):
             return [zeros(0, float32), zeros(0, float32)]
 
         segments_indices = self._get_segments_indices(peak_indices)
-        lowpass_segments = []
-        bandpass_segments = []
+
+        lowpass_series = []
+        bandpass_series = []
         for segment_indices in segments_indices:
             trough_indices = self._get_troughs(segment_indices, bandpass_signal)
 
-            lowpass_segments.append(self._get_beats(trough_indices, lowpass_signal))
-            bandpass_segments.append(self._get_beats(trough_indices, bandpass_signal))
+            lowpass_heartbeats = self._get_beats(trough_indices, lowpass_signal)
+            bandpass_heartbeats = self._get_beats(trough_indices, bandpass_signal)
 
-        lowpass_segments = stack(lowpass_segments)
-        bandpass_segments = stack(bandpass_segments)
-        print(lowpass_segments, bandpass_segments)
-        return lowpass_segments, bandpass_segments
+            lowpass_serie = self._sliding_window(lowpass_heartbeats)
+            bandpass_serie = self._sliding_window(bandpass_heartbeats)
+
+            if lowpass_serie.shape[0] > 1:
+                filtered_lowpass, filtered_bandpass = self._autocorrelation_filter(
+                    lowpass_heartbeats,
+                    lowpass_serie,
+                    bandpass_serie
+                )
+
+                lowpass_series.append(filtered_lowpass)
+                bandpass_series.append(filtered_bandpass)
+
+        if len(lowpass_series) == 0:
+            return [zeros(0, float32), zeros(0, float32)]
+
+        lowpass_series = concatenate(lowpass_series)
+        bandpass_series = concatenate(bandpass_series)
+        return lowpass_series, bandpass_series
 
     def _get_segments_indices(self, peak_indices):
         intervals = peak_indices[1:] - peak_indices[:-1]
@@ -120,3 +137,19 @@ class SegmentHeartbeats(PythonFunctionTransformOperation):
             beats[i] = beat_resampled
 
         return beats
+
+    def _sliding_window(self, signal):
+        hops = (signal.shape[0] - self.width + self.shift) // self.shift
+        window_idx = arange(0, self.width) + self.shift * reshape(arange(0, hops), (-1, 1))
+        balls = signal[window_idx]
+        return balls
+
+    def _autocorrelation_filter(self, lowpass_heartbeats, lowpass_series, bandpass_series):
+        template = lowpass_heartbeats.mean(axis=0)[newaxis, newaxis, :]
+        corr = correlate(template, lowpass_series, mode='valid')
+
+        valid_idx = numpy.all(corr > 0, axis=1)[:, 0]
+        filtered_lowpass = lowpass_series[valid_idx]
+        filtered_bandpass = bandpass_series[valid_idx]
+
+        return filtered_lowpass, filtered_bandpass
