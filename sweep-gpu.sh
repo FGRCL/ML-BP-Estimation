ce3cd938a1159af5d68d83b6cdae4452a118be31
François LaBerge <35940434+FGRCL@users.noreply.github.com>
2023-12-01 14:48:34 -0500

    Prepare sweeps (#113)
    * update the sweep job resources
    
    * decrease to 50 epochs

--- sweep-gpu.sh
+++ sweep-gpu.sh
@@ -2,10 +2,10 @@
 sbatch <<EOT
 #!/bin/sh
 #SBATCH --account def-bentahar
-#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-task:1
 #SBATCH --cpus-per-task=2  # Refer to cluster's documentation for the right CPU/GPU ratio
-#SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=2-00:00     # DD-HH:MM:SS
+#SBATCH --mem=125G       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --time=5-00:00     # DD-HH:MM:SS
 #SBATCH --array=1-$2
 #SBATCH --output=/home/fgrcl/scratch/job-logs/slurm-%A_%a.out
 
